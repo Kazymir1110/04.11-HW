8d12bf59a69ecbadfe6ba26e88c792f785a3795a
AndelKaz <149527003+AndelKaz@users.noreply.github.com>
2024-11-04 22:22:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+let celsia = prompt("Enter the Celsia");
+let faranheit = celsia * 9 / 5 + 32;
+console.log(faranheit);
+
+
+let daysPerMonth = 31;
+let hoursPerMonth = daysPerMonth * 24;
+let minPerMonth = hoursPerMonth * 60;
+console.log("Hours per month: " + hoursPerMonth );
+console.log("Min per month: " + minPerMonth );
+console.log("Days per month: " + daysPerMonth );
+
+let health = 100;
+let energy = 50;
+health -= 30;
+energy -= 27;
+console.log("Health " + health);
+console.log("Energy " + energy);
+
+
+let caseIn = 150;
+let caseIs = 150;
+caseIs = caseIs - (caseIn * (10/100));
+console.log("With 10% discount: " + caseIs);
+
+ 
+
+let flyingDot = parseFloat(8.21);
+flyingDot = Math.floor(flyingDot);
+console.log(flyingDot);
+
+
+let pfExample = "74.456789hsjj";
+pfExample = parseFloat(pfExample)
+console.log(pfExample)
+
+
+
+let piExample = "765432idbudsj";
+piExample = parseInt(piExample);
+console.log(piExample)
+
+
+let root = 169;
+root = Math.sqrt(root);
+console.log(root);
+
+
+let lineNumber = "47";
+let stringNumber = 62;
+
+let finalLineNum = lineNumber.toString();
+console.log( finalLineNum);
+
+let finalStringNum = parseInt(stringNumber);
+console.log(finalStringNum);
